2e4a1c132c87304f4ad9b43397864ad33520d05d
pepsakdoek <65312601+pepsakdoek@users.noreply.github.com>
2023-12-09 12:33:29 +0200

    Questions are now asked
    Linked the buttons, and "decoded" the query result to arrays, and selects 3, including the correct one.

--- scenes/MainUI.gd
+++ scenes/MainUI.gd
@@ -4,6 +4,10 @@ var base_db_path := "res://database/Zulu.db"
 var db_path := "user://database/Zulu.db"
 var db
 var settingsUIscene = load("res://scenes/SettingsUI.tscn")
+var native_word_list = []
+var foreign_word_list = []
+var question_word
+var answer_word
 
 
 # Called when the node enters the scene tree for the first time.
@@ -42,6 +46,7 @@ func _on_btn_refresh_question_pressed():
 	
 	var wordtypesql = "TRUE" if wordtype == "all" else "word_types like '%" + wordtype + "%'"
 	# Sort of starting to get the valid list of words, but not complete
+	# TODO: Add ways to ignore "known" words
 	var sql = "Select * from LanguageWordList lwl left join ( " + \
 		"SELECT native_language, " + \
 		"   foreign_language, " + \
@@ -50,14 +55,46 @@ func _on_btn_refresh_question_pressed():
 		"   max(time_answered) as lastanswered, " + \
 		"   JULIANDAY('now') - max(time_answered) as days_since_last_answered " + \
 		"from QuestionTransactions qt " + \
+		"group by native_language, foreign_language, question_word ) t on t.question_word = lwl.native_word " + \
 		"WHERE " + wordtypesql + \
-		"group by native_language, foreign_language, question_word ) t on t.question_word = lwl.native_word"
-	
-
+		"limit 10"
 	
 	db.open_db()
-	db.query("select native_word, foreign_word from LanguageWordList where " + wordtypesql + ";")
-	print(db.query_result)
+	db.query(sql)
+
+	
+	# load words into arrays (might be a way to just do this in one go?)
+	for i in db.query_result:
+		native_word_list.append(i["native_word"])
+		foreign_word_list.append(i["foreign_word"])
+	
+	var q = randi_range(0,9)
+	question_word = foreign_word_list[q]
+	answer_word = native_word_list[q]
+	
+	foreign_word_list.remove_at(q)
+	native_word_list.remove_at(q)
+	
+	var answer_list = []
+	answer_list.append(answer_word)
+	
+	# in 1 means 0 and 1 I think...
+	for i in 2:
+		q = randi_range(0,native_word_list.size()-1)
+		answer_list.append(native_word_list[q])
+		native_word_list.remove_at(q)
+	
+	$BaseHBox/BaseVBox/Question.text = question_word
+	
+	q = randi_range(0,answer_list.size()-1)
+	$BaseHBox/BaseVBox/A1.text = answer_list[q]
+	answer_list.remove_at(q)
+	q = randi_range(0,answer_list.size()-1)
+	$BaseHBox/BaseVBox/A2.text = answer_list[q]
+	answer_list.remove_at(q)
+	$BaseHBox/BaseVBox/A3.text = answer_list[0]
+
+	
 	
 
 func _on_btn_exit_pressed():
@@ -69,3 +106,20 @@ func _on_btn_settings_pressed():
 	# we don't want to constantly close/open the DB
 	get_tree().change_scene_to_packed(settingsUIscene)
 
+
+
+func answer_pressed(caller):
+	print(caller.text + " " + answer_word)
+	pass # Replace with function body.
+
+
+func _on_a_1_pressed():
+	answer_pressed($BaseHBox/BaseVBox/A1)
+
+
+func _on_a_2_pressed():
+	answer_pressed($BaseHBox/BaseVBox/A2)
+
+
+func _on_a_3_pressed():
+	answer_pressed($BaseHBox/BaseVBox/A3)
